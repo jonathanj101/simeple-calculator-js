ff55d7d999610f252aa6b15a28640a9a0b3c7a9b
jonathanj101 <jonatjimen@gmail.com>
2020-08-13 21:44:56 -0400

    fixed addition, working properly now

--- main.js
+++ main.js
@@ -1,80 +1,82 @@
-const result = document.querySelector('#result');
-const prevResult = document.querySelector('#result2');
-const btns = document.querySelector('div.btns');
+const result = document.querySelector("#result");
+const prevResult = document.querySelector("#result2");
+const btns = document.querySelector("div.btns");
 
-let calcValue = '';
-let scndValue = '';
+let calcValue = "";
+let scndValue = "";
 
 for (let child of btns.children) {
-  child.addEventListener('click', insert);
+  child.addEventListener("click", insert);
 }
 
 function insert(e) {
   let btnValue = e.target.value;
   switch (btnValue) {
-    case 'C':
-      result.value = '0';
-      prevResult.value = '';
-      calcValue = '';
-      scndValue = '';
+    case "C":
+      result.value = "0";
+      prevResult.value = "";
+      calcValue = "";
+      scndValue = "";
       break;
 
-    case '<':
+    case "<":
       calcValue += result.value;
       scndValue += prevResult.value;
       result.value = result.value.substr(0, result.value.length - 1);
       prevResult.value = prevResult.value.substr(0, prevResult.value.length - 1);
-      if (result.value == '' && prevResult.value == '') {
-        result.value = '0';
-        prevResult.value = '';
-        calcValue = '';
-        scndValue = '';
-
+      if (result.value == "" && prevResult.value == "") {
+        result.value = "0";
+        prevResult.value = "";
+        calcValue = "";
+        scndValue = "";
       }
 
       break;
 
-    case '*':
-      calcValue += result.value + '*';
-      scndValue += result.value + '*';
-      result.value = '';
+    case "*":
+      calcValue += result.value + "*";
+      scndValue += result.value + "*";
+      result.value = "";
       prevResult.value = calcValue;
       break;
-    case '/':
-      calcValue += result.value + '/';
-      scndValue += result.value + '/';
-      result.value = '';
+    case "/":
+      calcValue += result.value + "/";
+      scndValue += result.value + "/";
+      result.value = "";
       prevResult.value = calcValue;
       break;
 
-    case '-':
-      calcValue += result.value + '-';
-      scndValue += result.value + '-';
-      result.value = '';
+    case "-":
+      calcValue += result.value + " -";
+      scndValue += result.value + " -";
+      result.value = "";
       prevResult.value = calcValue;
-
-    case '+':
-      calcValue += result.value + ' + ';
-      result.value = '';
       break;
 
-    case '=':
-      if (result.value == '0') {
-        result.value =  'Result is Undefined';
-      } else if (prevResult.value == '0') {
-        prevResult.value = '';
-        scndValue = '';
+    case "+":
+      calcValue += result.value + " + ";
+      scndValue += result.value + "+";
+      result.value = "";
+      prevResult.value = calcValue;
+      break;
+
+    case "=":
+      if (result.value == "0") {
+        result.value = "Result is Undefined";
+      } else if (prevResult.value == "0") {
+        prevResult.value = "";
+        scndValue = "";
       }
 
       calcValue += result.value;
       scndValue += prevResult.value;
       result.value = eval(calcValue);
       prevResult.value = result.value;
-      calcValue = '';
-      scndValue = '';
+      calcValue = "";
+      scndValue = "";
       break;
 
     default:
-      result.value === '0' ? result.value = btnValue : result.value += btnValue;
+      result.value === "0" ? (result.value = btnValue) : (result.value += btnValue);
   }
 }
